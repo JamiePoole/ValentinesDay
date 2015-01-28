f5691c6a40880f65e4beca3546b533ebf29ef789
unknown <ben.carmichael@LON04DT121.corp.local>
2015-01-28 17:39:00 +0000

    added animation scss

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <head>
 	<title>Twitter Romance</title>
 	<link type="text/css" href="assets/css/style.css" rel="stylesheet" />
+	<script src="//use.typekit.net/sbe5mrr.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
 	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="assets/js/tweenlite.min.js"></script>
 	<script type="text/javascript" src="assets/js/tweenlite.css.min.js"></script>
@@ -17,7 +19,20 @@
 </head>
 <body>
 
-	<section id="send">
+	<div class="info-wrap">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis egestas erat, sagittis dapibus elit. Aliquam a accumsan quam. Vestibulum cursus quam mi, eu hendrerit augue consequat eu. Pellentesque id tristique arcu.</p>
+		<p>DISCLAIMER: Donec non enim in risus consequat interdum. Aenean ut ultrices lacus, non maximus massa.</p>
+	</div>
+	<div class="info-nav animated fadeInDown">
+		<a href="#" class="info">Info</a>
+	</div>
+	
+	<div class="intro">
+		<h1>Lorem ipsum dolor sit amet</h1>
+		<img src="assets/img/envelope.png" height="457" width="622">
+	</div>
+
+	<section class="site-main" id="send">
 		<div id="tweet-form">
 			<form id="send-tweet" method="post" action="post.php">
 				<input type="hidden" name="nonce_token" value="<?php echo $_SESSION['nonce']; ?>" />
@@ -29,12 +44,13 @@
 				<input type="text" id="tweet-message" name="tweet_message" />
 				<input type="submit" id="submit-tweet" />
 			</form>
-
 			<span><?php $_ut->getDelay($_tq->time()); ?></span>
 		</div>
 		<div id="tweet-image">
 		</div>
 	</section>
+
+	<div class="page-border"></div>
 
 </body>
 </html>
