4a75ccae77cdd3c1d4674e0f03999481fd93ef9b
Ben Carmichael <ben.carmichael@LON04DT121.corp.local>
2015-01-30 17:47:57 +0000

    styles

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// GO! 
+// This ready handler passes the $ alias in to avoid conflict with other libraries.
+// ------------------------------------------------------------
+jQuery(document).ready(function($) {
+
+	$('.nav-btn').hide();
+
+	$('#fullpage').fullpage({
+		//Navigation
+        menu: true,
+        anchors:['intro', 'what', 'send', 'info'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['intro', 'what', 'send', 'info'],
+		//Scrolling
+        loopBottom: false,
+        loopTop: false,
+        // Design
+        fixedElements: '.info-nav, .home-nav, .share-nav',
+        // Events
+        afterLoad: function(anchorLink, index){
+        	if(index == 3){
+            	$('.home-nav').removeClass('fadeOutUp').addClass('fadeInDown').show();
+            	$('.info-nav, .share-nav').removeClass('fadeOutDown').addClass('fadeInUp').show();
+            }
+            if(index == 1 || index == 2){
+            	$('.home-nav').removeClass('fadeInDown').addClass('fadeOutUp');
+            	$('.info-nav, .share-nav').removeClass('fadeInUp').addClass('fadeOutDown');
+            }
+        }
+	});
+
+	$('.intro-page, .what-page').click(function(e) {
+		$.fn.fullpage.moveSectionDown();
+	});
+
+	$('.heading').fitText(0.9);
+	$('.fit-text').fitText();
+
+	$('.step-2').addClass('inactive');
+	$('.step-1 .next-step').click(function(e){
+		e.preventDefault();
+		$('.step-1').removeClass('active').addClass('inactive');
+		$('.step-2').removeClass('inactive').addClass('active');
+	});
+	$('.step-2 .prev-step').click(function(e){
+		e.preventDefault();
+		$('.step-2').removeClass('active').addClass('inactive');
+		$('.step-1').removeClass('inactive').addClass('active');
+	});
+
+});
--- index.php
+++ index.php
@@ -10,47 +10,80 @@
 	<link type="text/css" href="assets/css/style.css" rel="stylesheet" />
 	<script src="//use.typekit.net/sbe5mrr.js"></script>
 	<script>try{Typekit.load();}catch(e){}</script>
-	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-	<script type="text/javascript" src="assets/js/tweenlite.min.js"></script>
-	<script type="text/javascript" src="assets/js/tweenlite.css.min.js"></script>
-	<script type="text/javascript" src="assets/js/jquery.gsap.min.js"></script>
-	<script type="text/javascript" src="assets/js/jquery.scrollmagic.min.js"></script>
-	<script type="text/javascript" src="assets/js/script.js"></script>
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.fittext.js"></script>
+	<script src="assets/js/jquery.fullPage.min.js"></script>
+	<script src="assets/js/jquery.promin.js"></script>	
 </head>
 <body>
 
-	<div class="info-wrap">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis egestas erat, sagittis dapibus elit. Aliquam a accumsan quam. Vestibulum cursus quam mi, eu hendrerit augue consequat eu. Pellentesque id tristique arcu.</p>
-		<p>DISCLAIMER: Donec non enim in risus consequat interdum. Aenean ut ultrices lacus, non maximus massa.</p>
+	<div class="home-nav nav-btn animated">
+		<a href="#intro">twitter crush</a>
 	</div>
-	<div class="info-nav animated fadeInDown">
-		<a href="#" class="info">Info</a>
+	<div class="info-nav nav-btn animated">
+		<a href="#info">info</a>
+	</div>
+	<div class="share-nav nav-btn animated">
+		<a href="#info">share</a>
+	</div>
+
+	<div id="fullpage">
+
+		<div class="section intro-page" data-anchor="intro">
+			<h1 class="heading">twitter crush</h1>
+		</div>
+
+		<div class="section what-page" data-anchor="what">
+			<h1 class="heading">tweet your twitter crush anonymously</h1>
+		</div>
+
+		<div class="section send-page" data-anchor="send">
+			<div id="tweet-form">
+				<form id="send-tweet" method="post" action="post.php">
+					<input type="hidden" name="nonce_token" value="<?php echo $_SESSION['nonce']; ?>" />
+					<div class="form-step step-1 active">
+						<label for="tweet-target">
+							<span class="fit-text">twitter name of your crush</span>
+						</label>
+						<div class="form-control">
+							<span class="form-prepend">@</span>
+							<input type="text" id="tweet-target" name="tweet_target" />
+							<a class="next-step" href="#">next</a>
+						</div>
+					</div>
+					<div class="form-step step-2 inactive">
+						<label for="tweet-message">
+							<span class="fit-text">tweet to <span id="target-name">twitterhandle</span></span>
+						</label>
+						<div class="form-control">
+							<textarea id="tweet-message" name="tweet_message" rows="3"></textarea>
+							<a class="prev-step" href="#">back</a> <input type="submit" id="submit-tweet" value="send tweet" />
+						</div>
+					</div>
+				</form>
+				<span><?php $_ut->getDelay($_tq->time()); ?></span>
+			</div>
+			<div id="tweet-image"></div>
+		</div>
+
+		<div class="section info-page" data-anchor="info">
+			<div class="pic">
+				<img src="assets/original/09_1148892.jpg">
+			</div>
+			<div class="copy">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis egestas erat, sagittis dapibus elit. Aliquam a accumsan quam. Vestibulum cursus quam mi, eu hendrerit augue consequat eu. Pellentesque id tristique arcu.</p>
+				<p>DISCLAIMER: Donec non enim in risus consequat interdum. Aenean ut ultrices lacus, non maximus massa.</p>
+			</div>
+		</div>
+
 	</div>
 	
-	<div class="intro">
-		<h1>Lorem ipsum dolor sit amet</h1>
-		<img src="assets/img/envelope.png" height="457" width="622">
-	</div>
+	<div class="page-border top"></div>
+	<div class="page-border right"></div>
+	<div class="page-border bottom"></div>
+	<div class="page-border left"></div>
 
-	<section class="site-main" id="send">
-		<div id="tweet-form">
-			<form id="send-tweet" method="post" action="post.php">
-				<input type="hidden" name="nonce_token" value="<?php echo $_SESSION['nonce']; ?>" />
-				<label for="tweet-target">Who is your Twitter Romance?</label><br />
-				<input type="text" id="tweet-target" name="tweet_target" />
-				<br />
-				<br />
-				<label for="tweet-message">Send Message to your <span id="target-name">Twitter Romance</span></label><br />
-				<input type="text" id="tweet-message" name="tweet_message" />
-				<input type="submit" id="submit-tweet" />
-			</form>
-			<span><?php $_ut->getDelay($_tq->time()); ?></span>
-		</div>
-		<div id="tweet-image">
-		</div>
-	</section>
-
-	<div class="page-border"></div>
+	<script src="assets/js/script.js"></script>	
 
 </body>
 </html>
