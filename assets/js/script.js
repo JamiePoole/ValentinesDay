294497c505f1241ca36b655da1438c98b5cd9a44
Ben Carmichael <Ben.Carmichael@LON04DT121.corp.local>
2015-02-06 15:29:08 +0000

    form countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@
 // This ready handler passes the $ alias in to avoid conflict with other libraries.
 // ------------------------------------------------------------
 jQuery(document).ready(function($) {
-
-	$('.info-nav, .home-nav, .share-nav').hide();
 		
 	$('#fullpage').fullpage({
 		//Navigation
@@ -11,15 +9,12 @@ jQuery(document).ready(function($) {
         anchors:['intro', 'send', 'share', 'info'],
         navigation: true,
         navigationPosition: 'right',
-        //navigationTooltips: ['intro', 'send', 'share', 'info'],
 		//Scrolling
 		css3: true,
 		scrollingSpeed: 1000,
         loopBottom: true,
         loopTop: false,
         scrollOverflow: true,
-        // Design
-        fixedElements: '.info-nav, .home-nav, .share-nav, .down-nav',
         // Events
         onLeave: function(index, nextIndex, direction){
             if(index == 1){
@@ -37,9 +32,49 @@ jQuery(document).ready(function($) {
         }
 	});
 
-	$('.down-nav .down-btn').click(function(e) {
+	$('.scroll-btn').click(function(e) {
 		e.preventDefault();
 		$.fn.fullpage.moveSectionDown();
 	});
+
+	// Submit the form
+	function processForm(e){ 
+		$.ajax({ 
+			url: 'post.php', 
+			dataType: 'text', 
+			type: 'post', 
+			contentType: 'application/x-www-form-urlencoded', 
+			data: $(this).serialize(), 
+			success: function( data, textStatus, jQxhr ){ 
+				//$('#response').html( data );
+				$('#tweet-target, #tweet-message').val('');
+				$('#char-count').text('');
+				$('#tweet-message').attr('maxlength', 120);
+				$('#thanks').html('Aw, thanks for spreading the love.<br>You just helped us donate £1 to Save the Children.');
+				$.fn.fullpage.moveSectionDown();
+			}, 
+			error: function( jqXhr, textStatus, errorThrown ){ 
+				console.log( errorThrown );
+			} 
+		}); 
+		e.preventDefault(); 
+	} 
+	$('#send-tweet').submit( processForm );
+
+	// Tweet character countdown	
+	$('#tweet-target').on('keyup', function(event){
+		var len = $(this).val().length;
+		$('#tweet-message').attr('maxlength', 139-len);
+	});
+    $('#tweet-message').on('keyup', function(event){
+    	var max = $(this).attr('maxlength');
+    	var len = $(this).val().length;
+  		if (len >= max) {
+    		$('#char-count').text("You've have reached the character limit!");
+  		} else {
+    		var characters = max - len;
+    		$('#char-count').text(characters + ' characters left');
+  		}
+    });
 
 });
