02b4752bfa46bc3d1c9324338216a95718bffb6c
Jamie Poole <Jamie.Poole@360i.com>
2015-02-06 11:40:12 +0000

    Webhook

--- gitpull.php
+++ gitpull.php
@@ -1,3 +1,19 @@
 <?php
+ignore_user_abort(true);
 
-`git pull`
+/* Check if request comes from GitHub IP Range
+ * As provided by: https://api.github.com/meta
+ */
+if(!array_key_exists('REMOTE_ADDR', $_SERVER))
+	throw new Exception('Missing remote address.');
+
+$rmIP = ip2long($_SERVER['REMOTE_ADDR']);
+$ghIP = ip2long('192.30.252.0');
+$ghMask = -1024;
+var_dump(($ghIP & $ghMask) === ($rmIP & $ghMask));
+
+if(!empty($_POST['payload'])){
+	$payload = json_decode($_POST['payload']);
+
+	shell_exex('git pull');
+}
