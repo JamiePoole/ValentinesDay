a3fc62626270fb7246b8c94cd3f3f273eb1a5580
Jamie Poole <Jamie.Poole@360i.com>
2015-02-06 11:58:13 +0000

    Webhook

--- gitpull.php
+++ gitpull.php
@@ -3,17 +3,52 @@ ignore_user_abort(true);
 
 /* Check if request comes from GitHub IP Range
  * As provided by: https://api.github.com/meta
+ * @return		boolean
  */
-if(!array_key_exists('REMOTE_ADDR', $_SERVER))
-	throw new Exception('Missing remote address.');
+function validIP(){
+	if(!array_key_exists('REMOTE_ADDR', $_SERVER))
+		throw new Exception('Missing remote address.');
 
-$rmIP = ip2long($_SERVER['REMOTE_ADDR']);
-$ghIP = ip2long('217.138.9.130');
-$ghMask = -1024;
-var_dump(($ghIP & $ghMask) === ($rmIP & $ghMask));
+	$rmIP = ip2long($_SERVER['REMOTE_ADDR']);
+	$ghIP = ip2long('192.30.252.0');
+	$ghMask = -1024;
 
-if(!empty($_POST['payload'])){
-	$payload = json_decode($_POST['payload']);
-
-	shell_exex('git pull');
+	return ($ghIP & $ghMask) === ($rmIP & $ghMask);
 }
+
+function validSignature($secret, $payload){
+	if(!array_key_exists('HTTP_X_HUB_SIGNATURE', $_SERVER))
+		throw new Exception('Missing X-Hub-Signature header. Did you configure a secret token in hook settings?');
+
+	return 'sha1=' . hash_hmac('sha1', $payload, $secret, false) === $_SERVER['HTTP_X_HUB_SIGNATURE'];
+}
+
+function processPayload(){
+	$type = $_SERVER['CONTENT_TYPE'];
+	if($type === 'application/x-www-form-urlencoded'):
+		if(!array_key_exists('payload', $_POST))
+			throw new Exception('Missing payload.');
+		$payload = filter_input(INPUT_POST, 'payload');
+	elseif($type === 'application/json'):
+		$payload = file_get_contents('php://input');
+	else:
+		throw new Exception('Unknown content type.');
+	endif;
+
+	$payload = json_decode($payload);
+
+	if($payload === null)
+		throw new Exception('Failed to decode JSON: ' . function_exists('json_last_error_msg') ? json_last_error_msg() : json_last_error());
+
+	return $payload;
+}
+
+if (validIP()):
+	if(array_key_exists('payload', $_POST) && !empty($_POST['payload'])){
+		$payload = processPayload();
+		$secret = '360iValentine';
+		if(validSignature($secret, $payload))
+			shell_exex('git pull');
+	}
+
+endif;
