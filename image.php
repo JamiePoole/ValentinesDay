51860af76a6d5e12ef0ebfced640be62af11444f
Jamie Poole <email@jamiepoole.me>
2015-02-09 19:24:11 +0000

    Test fallback

--- image.php
+++ image.php
@@ -1,7 +1,13 @@
 <?php
 require_once('require.php');
 
+if(!isset($_GET['user']) | !isset($_GET['message'])){
+	$_GET['user'] = 'Amy';
+	$_GET['message'] = 'I rllly like u! will u be my valentine?!?!?!';
+}
 $_gi->setDetails(htmlspecialchars($_GET['user']), htmlspecialchars($_GET['message']));
 $_gi->paintImage();
-header('Content-type: image/jpeg');
-imagejpeg($_gi->paintImage(), null, 95);
+header('Content-type: image/png');
+header('Content-Disposition: filename="valentines.png"');
+imagepng($_gi->paintImage(), null, 8);
+exit;
