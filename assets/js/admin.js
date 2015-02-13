5a1b273ec3550cad6afee3fcbb0095ecdcd1ba15
Jamie Poole <Jamie.Poole@360i.com>
2015-02-13 10:24:48 +0000

    admin js syntax

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($){
 		$.ajax({
 			url: $(this).attr('href'),
 		}).done(function(){
-			$(this).animate({backgroundColor: 'red'}, 'slow', function(){
+			$(this).animate({background: 'red'}, 'slow', function(){
 				$(this).remove();
 			});
 		});
