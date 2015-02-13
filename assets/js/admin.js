b9b162274bca25165794319f5ca9a7b22c8a88f9
Jamie Poole <Jamie.Poole@360i.com>
2015-02-13 15:23:39 +0000

    admin js syntax

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function($){
 			url: loc,
 			success: function(response){
 				var fullView = $(response).find('.full-view');
-				console.log(fullView);
+				$('.full-view').replaceWith(fullView);
 			},
 		});
 	}, refreshTime);
