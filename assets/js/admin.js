9ff07be6389879e6b729b2b2f01e55c4866a62ce
Jamie Poole <Jamie.Poole@360i.com>
2015-02-13 14:30:35 +0000

    admin js syntax

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,11 +2,12 @@ jQuery(document).ready(function($){
 	
 	$('.full-view table .actions a').on('click', function(e){
 		e.preventDefault();
+		var this = $(this);
 		$.ajax({
-			url: $(this).attr('href'),
-			success: function(){
-				console.log($(this));
-				$(this).closest('tr').remove();
+			url: this.attr('href'),
+			success: function(this)){
+				console.log(this);
+				this.closest('tr').remove();
 			},
 		});
 	});
