679db9686d9800853de3944c487415e870d6c3c9
Jamie Poole <email@jamiepoole.me>
2015-02-15 13:53:23 +0000

    Admin Twitter

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -14,29 +14,4 @@ jQuery(document).ready(function($){
 		});
 	});
 
-	function getUrlParameter(sParam){
-		var sPageURL = window.location.search.substring(1);
-		var sURLVariables = sPageURL.split('&');
-		for(var i = 0; i < sURLVariables.length; i++){
-			var sParameterName = sURLVariables[i].split('=');
-			if(sParameterName[0] == sParam){
-				return sParameterName[1];
-			}
-		}
-	}
-
-	if(getURLParameter('page') == 'dash' || getURLParameter('page') == 'queue' || typeof getURLParameter('page') != 'undefined'){
-		var refreshTime = 5000;
-		refresh = setInterval(function(){
-			var loc = window.location;
-			$.ajax({
-				url: loc,
-				success: function(response){
-					var fullView = $(response).find('#main-section');
-					$('#main-section').replaceWith(fullView);
-				},
-			});
-		}, refreshTime);
-	}
-
 });
